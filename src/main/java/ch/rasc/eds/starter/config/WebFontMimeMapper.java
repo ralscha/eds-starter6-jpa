d20b839c19d98bc6e854d0ed51d8766136d81a75
Ralph Schaer <ralphschaer@gmail.com>
2016-01-22 17:52:16 +0100

    Add a mime mapper for webfonts

---
+++ src/main/java/ch/rasc/eds/starter/config/WebFontMimeMapper.java
@@ -0,0 +1,22 @@
+package ch.rasc.eds.starter.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.MimeMappings;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebFontMimeMapper implements EmbeddedServletContainerCustomizer {
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
+		mappings.add("svg", "image/svg+xml");
+		mappings.add("eot", "application/vnd.ms-fontobject");
+		mappings.add("woff", "application/font-woff");
+		mappings.add("woff2", "application/font-woff2");
+		mappings.add("ttf", "application/font-sfnt");
+		mappings.add("otf", "application/font-sfnt");
+		container.setMimeMappings(mappings);
+	}
+}
