f6bb8689bcb2fe3fc81358a6a4ed729148e05390
Ralph Schaer <ralphschaer@gmail.com>
2016-02-29 08:55:17 +0100

    Upgrade libraries.

--- client/app/Application.js
+++ client/app/Application.js
@@ -6,28 +6,6 @@ Ext.define('Starter.Application', {
 	stores: [ 'Navigation', 'Languages', 'Authority' ],
 
 	constructor: function() {
-
-		// start fix. isEqual is missing in current version
-		Ext.Array.isEqual = function(a, b) {
-			if (a === b) {
-				return true;
-			}
-			if (a == null || b == null) {
-				return false;
-			}
-			if (a.length != b.length) {
-				return false;
-			}
-
-			for (var i = 0; i < a.length; ++i) {
-				if (a[i] !== b[i]) {
-					return false;
-				}
-			}
-			return true;
-		};
-		// end fix
-
 		// <debug>
 		Ext.Ajax.on('beforerequest', function(conn, options, eOpts) {
 			options.withCredentials = true;
