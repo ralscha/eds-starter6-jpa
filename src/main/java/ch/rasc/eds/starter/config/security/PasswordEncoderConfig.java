c4bc2c502810519c8b823f8e9c5003161554f1ce
Ralph Schaer <ralphschaer@gmail.com>
2016-10-25 17:09:51 +0200

    Polish

--- src/main/java/ch/rasc/eds/starter/config/security/PasswordEncoderConfig.java
+++ src/main/java/ch/rasc/eds/starter/config/security/PasswordEncoderConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class PasswordEncoderConfiguration {
+public class PasswordEncoderConfig {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
